b18f9ffeebfac8231cf55cae87ce8e58afe88ed7
McSkinnerOG <mcskinnerleaks@gmail.com>
2020-04-29 00:17:56 +1000

    Major Commit
    Almost all variants have been added along with spawn_rules and new 3D models.

---
+++ bridge/plugins/solved.utilities.console.js
@@ -0,0 +1,221 @@
+Bridge.registerPlugin({
+    author: "solvedDev",
+    version: "1.1.4",
+    name: "Console",
+    description: "Makes a console available to plugins."
+});
+
+class Console {
+    constructor() {
+        this.content = [];
+        this.registered_sidebar = true;
+        this.registered_footer = false;
+        this.new_logs = 0;
+        this.input = "";
+        this.current_open_sidebar = "Explorer";
+        this.context = {};
+
+        this.input_logic = [
+            {
+                type: "icon",
+                text: "mdi-send",
+                action: () => {
+                    this.evalInput();
+                }
+            },
+            {
+                type: "input",
+              	input: this.input,
+                action: {
+                  	default: (val) => {
+                    	this.input = val;
+                  	},
+                  	enter: () => {
+                    	this.evalInput();
+                      	this.input = "";
+                	}
+                }
+            }
+        ];
+
+        this.setupUI();
+    }
+
+    evalInput() {
+        try {
+            this.__internal__log__("> " + this.input, "grey", false);
+
+            let res = (function(input) { return eval(input); }).call(this.context, this.input);
+            this.content.push({ text: "< " });
+            this.log(res);
+        } catch(e) {
+            this.content.push({ text: "< ", color: "error" });
+            this.error(e.message);
+        }
+    }
+
+    getSidebar() {
+        return {
+            id: "solved.utilities.console.sidebar",
+            icon: "mdi-message-alert",
+            title: "Console",
+            toolbar: [
+                {
+                    display_icon: "mdi-cancel",
+                    display_name: "Clear Console",
+                    action: () => {
+                        this.clear();
+                    }
+                },
+                {
+                    display_icon: "mdi-open-in-new",
+                    display_name: "As Separate Window",
+                    action: () => {
+                        this.registered_sidebar = false;
+                        this.updateUI();
+                        Bridge.Window.open("solved.utilities.console.window");
+                        Bridge.Sidebar.remove("solved.utilities.console.sidebar");
+                    }
+                }
+            ],
+            content: this.getContent()
+        };
+    }
+    
+    setupUI(action_content=this.input_logic) {
+        Bridge.Sidebar.register(this.getSidebar());
+        Bridge.Window.register({
+            id: "solved.utilities.console.window",
+            display_name: "Console",
+            content: this.content,
+            options: {
+                is_draggable: true,
+                blurs_background: false,
+                is_closable: false
+            },
+            toolbar: [
+                {
+                    display_icon: "mdi-minus",
+                    action: () => {
+                        this.registered_sidebar = true;
+                        Bridge.Sidebar.register(this.getSidebar());
+                        this.updateUI();
+                        Bridge.Window.close("solved.utilities.console.window");
+                        Bridge.Sidebar.open("solved.utilities.console.sidebar");
+                    }
+                }
+            ],
+            actions: action_content
+        });
+        Bridge.on("bridge:openedSidebar", sidebar_id => {
+            this.current_open_sidebar = sidebar_id;
+            if(sidebar_id == "solved.utilities.console.sidebar") {
+                this.new_logs = 0;
+              	this.registered_footer = false;
+                Bridge.Footer.remove("solved.utilities.console.footer");
+            } 
+        });
+    }
+
+    updateUI() {
+        if(this.registered_sidebar) {
+            Bridge.Sidebar.update({
+                id: "solved.utilities.console.sidebar",
+                content: this.getContent()
+            }); 
+        } else {
+            Bridge.Window.update({
+                id: "solved.utilities.console.window",
+                content: this.getContent(),
+                actions: this.input_logic
+            });
+        }        
+    }
+
+    getContent() {
+        if(this.registered_sidebar) return this.content.concat([
+            {
+                type: "horizontal",
+                content: this.input_logic
+            }
+        ]);
+        return this.content;
+    }
+
+    __internal__log__(data, color, divider=true) {       
+        if(typeof data != "string") {
+            this.content.push({ text: JSON.stringify(data, null, "  ") + "\n", color });
+        } else {
+            this.content.push({ text: data + "\n", color });
+        }
+        
+        if(divider) this.content.push({ type: "divider" });
+        
+        this.updateUI();
+    }
+    __increase__badge__(new_logs=this.new_logs) {
+        if(!this.registered_sidebar|| this.current_open_sidebar == "solved.utilities.console.sidebar") return;
+
+        this.new_logs++;
+        if(!this.registered_footer) {
+            this.registered_footer = true;
+            Bridge.Footer.register({
+                id: "solved.utilities.console.footer",
+                display_name: `${new_logs+1} new logs`,
+                display_icon: "mdi-message-alert",
+                badge: {
+                    color: "orange",
+                    content: new_logs+1
+                },
+                action: () => {
+                    if(this.registered_sidebar) Bridge.Sidebar.open("solved.utilities.console.sidebar")
+                }
+            });
+        } else {
+            Bridge.Footer.update({
+                id: "solved.utilities.console.footer",
+                display_name: `${new_logs+1} new logs`,
+                badge: {
+                    content: new_logs+1
+                }
+            });
+        }
+    }
+
+    log(...args) {
+        args.forEach(log => {
+            this.__internal__log__(log);
+            this.__increase__badge__();
+        });
+    }
+    warn(...args) {
+        args.forEach(log => {
+            this.__internal__log__(log, "orange");
+            this.__increase__badge__();
+        });
+    }
+    error(...args) {
+        args.forEach(log => {
+            this.__internal__log__(log, "error");
+            this.__increase__badge__();
+        });
+    }
+  	info(...args) {
+        args.forEach(log => {
+            this.__internal__log__(log, "info");
+            this.__increase__badge__();
+        });
+    }
+  
+  	hook(context) {
+    	this.context = context;
+    }
+
+    clear() {
+        this.content = [];
+        this.updateUI();
+    }
+}
+
+provide(new Console())
+    .as("console");
---
+++ bridge/plugins/solved.utilities.file_search.js
@@ -0,0 +1,194 @@
+let SEARCH_INPUT = "";
+let SEARCH_CATEGORY = "entities";
+
+Bridge.registerPlugin({
+    author: "solvedDev",
+    version: "1.1.4",
+    name: "File Search",
+    description: "Quickly search all files of a project for specific keywords."
+});
+
+function loadProject(segment, cb) {
+    Bridge.FS.readDirectory(segment, (err, files) => {
+        if(err) console.warn(err);
+
+        let total = 0;
+        let list = [];
+      
+      	if(!files || files.length == 0) { cb([]); return; }
+        files.forEach(file => {
+            Bridge.FS.stats(`${segment}/${file}`, (err, stats) => {
+                if(err) throw err;
+                if(stats.isFile()) {
+                    Bridge.FS.readFile(`${segment}/${file}`, (err, data) => {
+                        if(err) console.warn(err);
+                        
+                        list.push({
+                            content: data.toString(),
+                            file_name: file.toLowerCase(),
+                          	file_path: `${Bridge.Utils.base_path}${segment}\\${file}`
+                        });
+    
+                        total++;
+                        if(total >= files.length) {
+                            if(cb) cb(list);
+                        }
+                    });
+                } else {
+                    loadProject(`${segment}/${file}`, (dir) => {
+                        if(dir) list.push(...dir);
+
+                        total++;
+                        if(total >= files.length) {
+                            if(cb) cb(list);
+                        }
+                    });
+                }
+            });            
+        });
+    });
+}
+
+function setupUI() {
+    Bridge.Window.register({
+        id: "solved.utilities.file_search.window",
+        options: {
+            height: 160,
+            is_persistent: false,
+            blurs_background: false,
+            is_frameless: true
+        },
+        content: [
+            {
+                type: "header",
+                text: "Information"
+            },
+            {
+                type: "divider"
+            },
+            {
+                text: "\nThis plugin scans all files (name & content) for the search term you enter.\n\n"
+            },
+            {
+                type: "divider"
+            },
+            {
+                text: "created by solvedDev",
+                color: "grey"
+            }
+        ]
+    });
+    Bridge.Sidebar.register({
+        id: "solved.utilities.file_search.sidebar",
+        icon: "mdi-magnify",
+        title: "File Search",
+        toolbar: [
+            {
+                display_name: "Information",
+                display_icon: "mdi-information",
+                action() {
+                    Bridge.Window.open("solved.utilities.file_search.window");
+                }
+            }
+        ],
+        content: [
+            {
+                type: "select",
+                input: SEARCH_CATEGORY,
+                options: [
+                    "entities",
+                    "loot_tables",
+                    "functions",
+                    "trading"
+                ],
+                action(val) {
+                    SEARCH_CATEGORY = val;
+                    if(SEARCH_INPUT != "") loadProject(SEARCH_CATEGORY, updateUI);
+                }
+            },
+            {
+                type: "divider"
+            },
+            {
+                type: "input",
+                text: "Search...",
+                action(val) {
+                    SEARCH_INPUT = val;
+                    updateUI();
+                    loadProject(SEARCH_CATEGORY, updateUI);
+                }
+            },
+            {
+                text: "Start typing to search...",
+                color: "grey"
+            }
+        ]
+    });
+}
+
+function updateUI(list) {
+    let content = [
+        {
+            type: "select",
+            input: SEARCH_CATEGORY,
+            options: [
+                "entities",
+                "loot_tables",
+                "functions",
+                "trading"
+            ],
+            action(val) {
+                SEARCH_CATEGORY = val;
+                if(SEARCH_INPUT != "") loadProject(SEARCH_CATEGORY, updateUI);
+            }
+        },
+        {
+            type: "divider"
+        },
+        {
+            type: "input",
+            text: "Search...",
+          	key: Math.random(),
+          	input: SEARCH_INPUT,
+            action(val) {
+                SEARCH_INPUT = val;
+                updateUI();
+                loadProject(SEARCH_CATEGORY, updateUI);
+            }
+        }
+    ];
+
+    if(!list) {
+        content.push({ type: "loader" });
+    } else if(SEARCH_INPUT != "") {
+      	list.sort((a, b) => {
+          if(a.file_name > b.file_name) return 1;
+          if(a.file_name < b.file_name) return -1;
+          return 0;
+        });
+        list.forEach(el => {
+            if(el.content.includes(SEARCH_INPUT) || el.file_path.toLowerCase().includes(SEARCH_INPUT)) {
+                content.push({
+                  	text: el.file_name + "\n",
+                  	color: "success",
+                  	action: () => {
+                        console.log(el.file_path)
+                		Bridge.openFile(el.file_path);
+                	} 
+                });
+            }
+        });
+    } else {
+        content.push({ text: "Start typing to search...", color: "grey" });
+    }
+    if(content.length <= 3) {
+        content.push({ text: "No results found for \"" }, { text: SEARCH_INPUT, color: "error" }, { text: "\"" });
+    }
+    
+    Bridge.Sidebar.update({
+        id: "solved.utilities.file_search.sidebar",
+        content
+    });
+}
+
+setupUI();
